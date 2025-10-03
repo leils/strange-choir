488009427a8b5dd5b7c50dfe9fb905e5591daf01
Leia <e.leiachang@gmail.com>
2025-10-02 21:26:11 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'AI Poet Chatbot',
-  description: 'A chatbot that speaks in poetic verse',
+  title: 'An uncanny mirror',
+  description: 'Templated chat bot trained on my stream of conscious writings - lsc',
 };
 
 export default function RootLayout({
